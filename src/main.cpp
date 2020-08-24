10a0f27652196fb9d309555924c48c8194f663a2
Marco Tan <marco.tan.200405@gmail.com>
2020-08-23 20:09:35 -0400

    Kinda excessive to use a lambda for every value.

--- src/main.cpp
+++ src/main.cpp
@@ -4,54 +4,26 @@
 
 #define DRIVE_ARCADE
 
+auto Compare_Vals(int x, int y) -> int
+{
+    if (x >= y)
+        return x;
+    else
+        return 0;
+}
+
 auto Drive_Ctrl() -> void
 {
     while (true)
     {
 #if defined DRIVE_ARCADE
-        int power {
-            [](int i)
-            {
-                if (i >= misc::ctrlDeadzone)
-                    return i;
-                else
-                    return 0;
-            }(ctrl_master.get_analog(misc::ctrlAnalog::E_CONTROLLER_ANALOG_RIGHT_Y))
-        };
-
-        int turn {
-            [](int i)
-            {
-                if (i >= misc::ctrlDeadzone)
-                    return i;
-                else
-                    return 0;
-            }(ctrl_master.get_analog(misc::ctrlAnalog::E_CONTROLLER_ANALOG_RIGHT_X))
-        };
-
+        int power {Compare_Vals(ctrl_master.get_analog(misc::ctrlAnalog::E_CONTROLLER_ANALOG_RIGHT_Y))}, 
+            turn {Compare_Vals(ctrl_master.get_analog(misc::ctrlAnalog::E_CONTROLLER_ANALOG_RIGHT_X))};
         Set_Powers(power + turn, power - turn);
 
 #elif defined DRIVE_TANK
-        int power_left {
-            [](int i)
-            {
-                if (i >= misc::ctrlDeadzone)
-                    return i;
-                else
-                    return 0;
-            }(ctrl_master.get_analog(misc::ctrlAnalog::E_CONTROLLER_ANALOG_LEFT_Y))
-        };
-
-        int power_right {
-            [](int i)
-            {
-                if (i >= misc::ctrlDeadzone)
-                    return i;
-                else
-                    return 0;
-            }(ctrl_master.get_analog(misc::ctrlAnalog::E_CONTROLLER_ANALOG_RIGHT_Y))
-        };
-
+        int power_left {Compare_Vals(ctrl_master.get_analog(misc::ctrlAnalog::E_CONTROLLER_ANALOG_LEFT_Y))}, 
+            power_right {Compare_Vals(ctrl_master.get_analog(misc::ctrlAnalog::E_CONTROLLER_ANALOG_RIGHT_Y))};
         Set_Powers(power_left, power_right);
 #endif
         pros::delay(5);
